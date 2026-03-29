fcd11ea23b99cd04298bef92ca7205e40855246a
Jorgulas <dincao17@gmail.com>
2026-03-29 16:34:54 +0100

    Small Legibility Fix

--- src/RunAll.kt
+++ src/RunAll.kt
@@ -6,23 +6,27 @@ const val NEED_COUNT_4_Ex2 = false
 
 fun main() {
     val fileInput = "SRR494099.fastq.gz"
-    val k = 6
+    val filename = fileInput.substringBefore(".fastq.gz")
+    val k = 3
 
     measureAndPrintTime ("Finished running all exercises") {
         println("// Running Ex1: k-mer counting")
-        val ex1Output = "results/Result_1_" + fileInput.substring(0, fileInput.length-9) + ".csv"
+        val ex1Output = "results/Result_1_$filename.csv"
         Ex1.run(fileInput, ex1Output, k)
         println()
+
         println("// Running Ex2: De Bruijn graph construction")
-        val ex2Output = "results/Result_2_" + fileInput.substring(0, fileInput.length-9) + ".csv"
+        val ex2Output = "results/Result_2_$filename.csv"
         Ex2.run(ex1Output, ex2Output)
         println()
+
         println("// Running Ex3: Eulerian path finding")
-        val ex3Output = "results/Result_3_" + fileInput.substring(0, fileInput.length-9) + ".csv"
+        val ex3Output = "results/Result_3_$filename.csv"
         Ex3.run(ex2Output, ex3Output)
         println()
+
         println("// Running Ex4: Contig extraction")
-        val ex4Output = "results/Result_4_" + fileInput.substring(0, fileInput.length-9) + ".csv"
+        val ex4Output = "results/Result_4_$filename.csv"
         Ex4.run(ex2Output, ex4Output)
         println()
     }
